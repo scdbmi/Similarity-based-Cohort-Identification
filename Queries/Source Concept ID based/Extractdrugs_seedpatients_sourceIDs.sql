62dc1094ab293c7a67f34068e2dbc37c8a1df3ad
scdbmi <shreyachakrabarti@gmail.com>
2017-01-24 16:25:34 -0500

    Delete Extractdrugs_seedpatients_sourceIDs.sql

--- Queries/Source Concept ID based/Extractdrugs_seedpatients_sourceIDs.sql
+++
@@ -1,8 +0,0 @@
-SELECT [person_id]
-      ,[drug_source_value]
-  /* Enter appropriate OMOP data table name below*/ 
-  FROM [ohdsi].[west].[drug_exposure]
-  WHERE person_id in (
-  /* Insert list of seed patients in the following format for m seed patients*/
-  /*'person_ids1','person_ids2',...'person_idsm'*/
-  )
