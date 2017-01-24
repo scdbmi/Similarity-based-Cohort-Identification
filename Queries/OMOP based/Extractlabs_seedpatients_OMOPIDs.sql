4470b157dcbd5206886338a50473c15b8a7e981c
scdbmi <shreyachakrabarti@gmail.com>
2017-01-24 16:24:45 -0500

    Delete Extractlabs_seedpatients_OMOPIDs.sql

--- Queries/OMOP based/Extractlabs_seedpatients_OMOPIDs.sql
+++
@@ -1,9 +0,0 @@
-SELECT DISTINCT [person_id]
-      ,[value_as_number]
-	  ,[measurement_concept_id]
-  /* Enter appropriate OMOP data table name below*/ 
-  FROM [ohdsi].[west].[measurement]
-  WHERE person_id in (
-  /* Insert list of seed patients in the following format for m seed patients*/
-  /*'person_ids1','person_ids2',...'person_idsm'*/
-  )
