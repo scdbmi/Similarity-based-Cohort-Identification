68fa34040fccd257ee4ae07c97c1e82ec6ab05d3
scdbmi <shreyachakrabarti@gmail.com>
2017-01-24 16:25:14 -0500

    Delete Extractconditions_seedpatients_sourceIDs.sql

--- Queries/Source Concept ID based/Extractconditions_seedpatients_sourceIDs.sql
+++
@@ -1,8 +0,0 @@
-SELECT [person_id]
-      ,[condition_source_value]
-  /* Enter appropriate OMOP data table name below*/ 
-  FROM [ohdsi].[west].[condition_occurrence]
-  WHERE person_id in (
-  /* Insert list of seed patients in the following format for m seed patients*/
-  /*'person_ids1','person_ids2',...'person_idsm'*/
-  )
